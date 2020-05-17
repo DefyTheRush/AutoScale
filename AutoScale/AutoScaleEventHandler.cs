1b602ae45fe18d29afb4ca7b3380270347518893
Hashim <42122270+DefyTheRush@users.noreply.github.com>
2020-05-17 15:02:58 -0400

    Add files for AutoScale V1

---
+++ AutoScale/AutoScaleEventHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using EXILED;
+using EXILED.Extensions;
+using MEC;
+
+namespace TinyMe
+{
+    public class AutoScaleEventHandler
+    {
+        HashSet<String> FirstJoinedPlayers = new HashSet<String>();
+        public void RunWhenRoundStarts()
+        {
+            foreach (ReferenceHub Player in Player.GetHubs())
+            {
+                Map.Broadcast($"Everyone who joined has their playermodel scale set to {AutoScaleMeMain.ScaleValue}x!", 5);
+                FirstJoinedPlayers.Add(Player.characterClassManager.UserId);
+                Player.SetScale(AutoScaleMeMain.ScaleValue);
+            }
+        }
+
+        public void RunWhenRoundEnds()
+        {
+            FirstJoinedPlayers.Clear();
+        }
+
+        public void RunWhenPlayerJoins(PlayerJoinEvent PlayerJoin)
+        {
+            if (FirstJoinedPlayers.Contains(PlayerJoin.Player.characterClassManager.UserId))
+            {
+                PlayerJoin.Player.Broadcast(5, $"Your playermodel scale was set to {AutoScaleMeMain.ScaleValue}x!", false);
+                PlayerJoin.Player.SetScale(AutoScaleMeMain.ScaleValue);
+            }
+        }
+    }
+}
---
+++ AutoScale/AutoScaleMeMain.cs
@@ -0,0 +1,46 @@
+﻿using EXILED;
+using System;
+
+namespace TinyMe
+{
+    public class AutoScaleMeMain : EXILED.Plugin
+    {
+        public override string getName => "TinyMe";
+        public static float ScaleValue;
+        public AutoScaleEventHandler Handler;
+        bool EnablePlugin;
+
+        public void ReloadCofiguration()
+        {
+            EnablePlugin = Config.GetBool("autoscale_enable", false);
+            if (EnablePlugin)
+                Log.Info("Enabling \"AutoScale\"!");
+            else
+                Log.Info("Disabling \"AutoScale\"!");
+
+            ScaleValue = Config.GetFloat("autoscale_value", 1f);
+        }
+
+        public override void OnDisable()
+        {
+            Events.RoundEndEvent -= Handler.RunWhenRoundEnds;
+            Events.RoundStartEvent -= Handler.RunWhenRoundStarts;
+            Events.PlayerJoinEvent -= Handler.RunWhenPlayerJoins;
+            Handler = null;
+        }
+
+        public override void OnEnable()
+        {
+            ReloadCofiguration();
+            if (!EnablePlugin)
+                return;
+
+            Handler = new AutoScaleEventHandler();
+            Events.PlayerJoinEvent += Handler.RunWhenPlayerJoins;
+            Events.RoundStartEvent += Handler.RunWhenRoundStarts;
+            Events.RoundEndEvent += Handler.RunWhenRoundEnds;
+        }
+
+        public override void OnReload() { }
+    }
+}
